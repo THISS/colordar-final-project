39cb9c93549f3a2e362ead73165ad050d4d4882d
Brenton Holswich <brenton.holswich@gmail.com>
2017-04-25 05:55:12 +0000

    trying to fix sass loader error on heroku

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -54,7 +54,8 @@ module.exports = {
       comments: false
     }),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
+      'process.env.NODE_ENV': JSON.stringify('production'),
+      BROWSER: JSON.stringify(true)
     })
   ]
 };

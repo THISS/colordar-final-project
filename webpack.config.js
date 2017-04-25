425996a90702d3e1fe9b0d5aac260c357d256560
Brenton Holswich <brenton.holswich@gmail.com>
2017-04-25 01:51:25 +0000

    had something not work on heroku so removed it

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,6 @@ module.exports = {
   },
   plugins: debug ? [] : [
     new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({
       beautify: false,
       mangle: {

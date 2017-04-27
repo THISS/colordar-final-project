88a9774cf65b872e4ef1c64955ac4691e8158cfc
Jan Wong <janwong94@gmail.com>
2017-04-27 20:02:58 +0000

    added click action to today events section

---
+++ client/src/js/actions/index.js
@@ -0,0 +1,9 @@
+
+// Create the action controller which returns the ACTION within the function
+export const selectEvent = (event) => {
+  console.log("You clicked on event:", event.title);
+  return {
+    type: "EVENT_SELECTED",
+    payload: event
+  }
+}

31cfca5ffa76fc28c473fc2779e90ecdd77e4c65
Brenton Holswich <brenton.holswich@gmail.com>
2017-04-27 08:07:37 -0700

    testing dotenv change

--- back-server.js
+++ back-server.js
@@ -1,9 +1,8 @@
 'use strict';
+require('dotenv').config();
 
 const ENV = process.env.ENV || 'development';
-if (ENV === 'development') {
-  require('dotenv').config();
-}
+
 const PORT = process.env.PORT || 8080;
 const express = require('express');
 const app = express();

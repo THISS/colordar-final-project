775f7cfc30bd0d6a7556f368bf13de0c06c10211
Jan Wong <janwong94@gmail.com>
2017-05-04 10:51:49 +0000

    finished styling groups page

--- client/src/js/components/GroupsList.jsx
+++ client/src/js/components/GroupsList.jsx
@@ -5,42 +5,53 @@ class GroupsList extends Component {
     return(
       <div className="groups-list">
         <div className="single-group">
-          <span className="group-circle"></span>
+          <span className="group-circle one">
+            <img className="squiggle" src="/images/not-busy-icon.png" alt="squiggle"/>
+          </span>
           <div className="group-label">
             <p>The hiking group</p>
           </div>
         </div>
 
         <div className="single-group">
-          <span className="group-circle"></span>
+          <span className="group-circle two"></span>
           <div className="group-label">
             <p>The hiking group</p>
           </div>
         </div>
 
         <div className="single-group">
-          <span className="group-circle"></span>
+          <span className="group-circle three">
+            <img className="squiggle" src="/images/busy-icon.png" alt="squiggle"/>
+          </span>
           <div className="group-label">
             <p>The hiking group</p>
           </div>
         </div>
 
         <div className="single-group">
-          <span className="group-circle"></span>
+          <span className="group-circle four">
+            <span className="notification notification-active">3</span>
+          </span>
           <div className="group-label">
             <p>The hiking group</p>
           </div>
         </div>
 
         <div className="single-group">
-          <span className="group-circle"></span>
+          <span className="group-circle five">
+            <img className="squiggle" src="/images/kinda-busy-icon.png" alt="squiggle"/>
+          </span>
           <div className="group-label">
             <p>The hiking group</p>
           </div>
         </div>
 
         <div className="single-group">
-          <span className="group-circle"></span>
+          <span className="group-circle six">
+            <span className="notification notification-active">2</span>
+            <img className="squiggle" src="/images/busy-icon.png" alt="squiggle"/>
+          </span>
           <div className="group-label">
             <p>The hiking group</p>
           </div>

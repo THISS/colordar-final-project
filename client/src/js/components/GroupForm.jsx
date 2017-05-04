65cca5637e19c5b96052d7eb552a4ce30f4c8e6e
Jan Wong <janwong94@gmail.com>
2017-05-04 09:21:01 +0000

    started styling groups page

---
+++ client/src/js/components/GroupForm.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class GroupForm extends Component {
+  render() {
+    return(
+      <div className="group-form">
+
+      </div>
+    );
+  }
+}
+
+export default GroupForm;
---
+++ client/src/js/components/GroupsList.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+class GroupsList extends Component {
+  render() {
+    return(
+      <div className="groups-list">
+        <div className="single-group">
+          <span className="group-circle"></span>
+          <div className="group-label">
+            <p>The hiking group</p>
+          </div>
+        </div>
+
+        <div className="single-group">
+          <span className="group-circle"></span>
+          <div className="group-label">
+            <p>The hiking group</p>
+          </div>
+        </div>
+
+        <div className="single-group">
+          <span className="group-circle"></span>
+          <div className="group-label">
+            <p>The hiking group</p>
+          </div>
+        </div>
+
+        <div className="single-group">
+          <span className="group-circle"></span>
+          <div className="group-label">
+            <p>The hiking group</p>
+          </div>
+        </div>
+
+        <div className="single-group">
+          <span className="group-circle"></span>
+          <div className="group-label">
+            <p>The hiking group</p>
+          </div>
+        </div>
+
+        <div className="single-group">
+          <span className="group-circle"></span>
+          <div className="group-label">
+            <p>The hiking group</p>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default GroupsList;

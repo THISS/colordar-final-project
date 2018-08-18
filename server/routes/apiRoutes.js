0036794b569f3ea2cc345d96f0cb1a2e6f8bd113
Brenton Holswich <brenton.holswich@gmail.com>
2018-08-17 18:06:46 -0700

    made it so there is auth middleware to routes only a user should access

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,43 +1,31 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const isLoggedIn = require("../lib/auth/auth");
 
-// returns a function of route middleware to indicate whether user is logged in or not
-function isLoggedIn(logger) {
-  
-  return (req, res, next) => {
-    // if user is authenticated in the session, carry on
-    logger.log('authenticated', req.isAuthenticated());
-    
-    if (req.isAuthenticated()) {
-      res.locals.loggedIn = true;
-    }else {
-      // if they aren't redirect them to the home page
-      res.locals.loggedIn = false;
-      res.json(res.locals);
-      return;
-    }
-    return next();
-
-  }
-}
-
-module.exports = function(dbHelpers, passport, logger) {
+module.exports = (dbHelpers, passport, logger) => {
   const isUserLoggedIn = isLoggedIn(logger);
   // Gather the api routers
-  const users = require('./_usersApi')(dbHelpers.users, passport, logger, isUserLoggedIn);
+  const users = require("./_usersApi")(
+    dbHelpers.users,
+    passport,
+    logger,
+    isUserLoggedIn
+  );
 
-  const calendars = require('./_calendarsApi')(dbHelpers, logger);
-  const groups = require('./_groupsApi')(dbHelpers, logger);
-  const events = require('./_eventsApi')(dbHelpers.events, logger);
-  
-  // Mount the Routes on Api
-  router.use('/users', users);
+  const calendars = require("./_calendarsApi")(dbHelpers, logger);
+  const groups = require("./_groupsApi")(dbHelpers, logger);
+  const events = require("./_eventsApi")(dbHelpers.events, logger);
 
+  // unprotected routes
+  router.use("/users", users);
+
+  // middleware
   router.use(isUserLoggedIn);
-  router.use('/calendars', calendars);
-  router.use('/groups', groups);
-  router.use('/events', events);
 
+  // protected routes
+  router.use("/calendars", calendars);
+  router.use("/groups", groups);
+  router.use("/events", events);
 
   return router;
-}
+};

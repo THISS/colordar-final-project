219d528ebdc99271a5afd82a765665f57ef223a3
Brenton Holswich <brenton.holswich@gmail.com>
2018-08-17 18:07:23 -0700

    cleaned up the passport config

--- config/passportconfig.js
+++ config/passportconfig.js
@@ -1,88 +1,102 @@
-
-const LocalStrategy = require('passport-local').Strategy;
-const SIGNUPMESSAGE = 'signupMessage';
-const LOGINMESSAGE = 'loginMessage';
-const BADLOGINMESSAGE = 'Either the email or password are incorrect';
-const BADSIGNUPMESSAGE = `We are struggling to process this request,
-you may want to check the password or if you already have an account`;
+const LocalStrategy = require("passport-local").Strategy;
+const SIGNUPMESSAGE = "signupMessage";
+const LOGINMESSAGE = "loginMessage";
+const BADLOGINMESSAGE = "Either the email or password are incorrect";
+const BADSIGNUPMESSAGE = "We are struggling to process this request";
 
 module.exports = (passport, dbHelpers, log) => {
-
-  passport.serializeUser(function(user, done) {
+  passport.serializeUser((user, done) => {
     done(null, user.id);
   });
 
-  passport.deserializeUser(function(id, done) {
-    dbHelpers.users.getUserById(id)
-      .then((user) => {
+  passport.deserializeUser((id, done) => {
+    dbHelpers.users
+      .getUserById(id)
+      .then(user => {
         done(null, user);
       })
-      .catch((err) => {
+      .catch(err => {
         log.error(err);
         done(err);
       });
   });
 
   // Local Signup
-  passport.use('local-signup', new LocalStrategy({
-      // by default local strat uses username and password, we have email
-      usernameField: 'email',
-      passwordField: 'password',
-      passReqToCallback: true // allows us to give the request object which may come in handy
-    },
-    function(req, email, password, done) {
-      // passwords must match
-      const { password_confirmation, first_name } = req.body;
+  passport.use(
+    "local-signup",
+    new LocalStrategy(
+      {
+        // by default local strat uses username and password, we have email
+        usernameField: "email",
+        passwordField: "password",
+        passReqToCallback: true // allows us to give the request object which may come in handy
+      },
+      (req, email, password, done) => {
+        // passwords must match
+        const { password_confirmation, first_name } = req.body;
 
-      if (password_confirmation !== password) {
-        return done(null, false, req.flash(SIGNUPMESSAGE, BADSIGNUPMESSAGE));
-      }
-      // email should not already exist
-      dbHelpers.users.getUserByEmail(email)
-        .then((user) => {
-          if (user.length > 0) {
-            return done(null, false, req.flash(SIGNUPMESSAGE, BADSIGNUPMESSAGE));
-          }
-          // other wise we need to create this user
-          return dbHelpers.users.registerUserLocal({
-            first_name,
-            email,
-            password
+        if (password_confirmation !== password) {
+          return done(null, false, BADSIGNUPMESSAGE);
+        }
+        // email should not already exist
+        dbHelpers.users
+          .getUserByEmail(email)
+          .then(user => {
+            if (user.length > 0) {
+              return done(null, false, BADSIGNUPMESSAGE);
+            }
+            // other wise we need to create this user
+            return dbHelpers.users.registerUserLocal({
+              first_name,
+              email,
+              password
+            });
+          })
+          .then(addedSuccessfully => {
+            if (addedSuccessfully) {
+              log.info(`addedSuccessfully - passportconfig.js line 57: ${addedSuccessfully[0]}`);
+              return done(null, addedSuccessfully[0]);
+            }
+          })
+          .catch(err => {
+            return done(err);
           });
-        })
-        .then((addedSuccessfully) => {
-          if (addedSuccessfully) {
-            return done(null, addedSuccessfully[0]);
-          }
-        })
-        .catch((err) => {
-          return done(err);
-        });
-    }
-  ));
+      }
+    )
+  );
 
   // Local Login
-  passport.use('local-login', new LocalStrategy({
-      usernameField: 'email',
-      passwordField: 'password',
-      passReqToCallback: true
-    },
-    function(req, email, password, done) {
-      dbHelpers.users.getUserByEmail(email)
-        .then((userResponse) => {
-          if (userResponse.length < 1) {
-            return done(null, false, req.flash(LOGINMESSAGE, BADLOGINMESSAGE));
-          }
-          if (!dbHelpers.users.comparePassword(password, userResponse[0].password_digest)) {
-            return done(null, false, req.flash(LOGINMESSAGE, BADLOGINMESSAGE));
-          }
-          // Return our user
-          return done(null, userResponse[0]);
-        })
-        .catch((err) => {
-          log.error(err);
-          return done(err);
-        });
-    }
-  ));
-}
+  passport.use(
+    "local-login",
+    new LocalStrategy(
+      {
+        usernameField: "email",
+        passwordField: "password",
+        passReqToCallback: true
+      },
+      (req, email, password, done) => {
+        dbHelpers.users
+          .getUserByEmail(email)
+          .then(userResponse => {
+            if (userResponse.length < 1) {
+              return done(null, false, BADLOGINMESSAGE);
+            }
+            if (
+              !dbHelpers.users.comparePassword(
+                password,
+                userResponse[0].password_digest
+              )
+            ) {
+              return done(null, false, BADLOGINMESSAGE);
+            }
+            // Return our user
+            return done(null, userResponse[0]);
+          })
+          .catch(err => {
+            log.error(err);
+            return done(err);
+          });
+      }
+    )
+  );
+};

b2249acf4b69b4b4f97159dfb05624a8f9ecae86
Brenton Holswich <brenton.holswich@gmail.com>
2018-08-17 22:03:18 -0700

    knex now works with the docker postgres

--- knexfile.js
+++ knexfile.js
@@ -5,13 +5,14 @@ if (process.env.NODE_ENV !== 'production') {
 module.exports = {
   development: {
     client: 'pg',
-    connection: {
-      host     : process.env.DB_HOST,
-      user     : process.env.DB_USER,
-      password : process.env.DB_PASSWORD,
-      database : process.env.DB_NAME,
-      port     : process.env.DB_PORT
-    },
+    connection: `${process.env.POSTGRES_URL}`,
+    // connection: {
+    //   host     : process.env.DB_HOST,
+    //   user     : process.env.DB_USER,
+    //   password : process.env.DB_PASSWORD,
+    //   database : process.env.DB_NAME,
+    //   port     : process.env.DB_PORT
+    // },
      pool: {
       min: 2,
       max: 10

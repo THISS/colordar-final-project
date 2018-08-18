a59aa887012e9c28e53a80ca6fe5a3d15de7c957
Brenton Holswich <brenton.holswich@gmail.com>
2018-08-18 00:30:14 -0700

    Getting a warning about promises so added return statements. didn't fix it.

--- server/lib/auth/auth.js
+++ server/lib/auth/auth.js
@@ -2,16 +2,16 @@
 module.exports = function isLoggedIn(logger) {
   return (req, res, next) => {
     // if user is authenticated in the session, carry on
-    logger.info("authenticated: ", req.isAuthenticated());
+    const isAuthenticated = req.isAuthenticated();
+    logger.info(`authenticated:  ${isAuthenticated}`);
 
-    if (req.isAuthenticated()) {
+    if (isAuthenticated) {
       res.locals.loggedIn = true;
+      return next();
     } else {
       // if they aren't redirect them to the home page
       res.locals.loggedIn = false;
-      res.json(res.locals);
-      return;
+      return res.json(res.locals);
     }
-    return next();
   };
 };

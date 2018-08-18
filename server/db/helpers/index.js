2006ab81759cf0a58c84495e33717eb01ddccb09
Brenton Holswich <brenton.holswich@gmail.com>
2018-08-18 00:31:54 -0700

    passing in the logger to the database helpers

--- server/db/helpers/index.js
+++ server/db/helpers/index.js
@@ -1,9 +1,9 @@
 // This is the database helper file that pulls all of the helpers together
-module.exports = function(knex) {
-  const users = require('./_users')(knex);
-  const groups = require('./_groups')(knex);
-  const calendars = require('./_calendars')(knex);
-  const events = require('./_events')(knex);
+module.exports = function(knex, logger) {
+  const users = require('./_users')(knex, logger);
+  const groups = require('./_groups')(knex, logger);
+  const calendars = require('./_calendars')(knex, logger);
+  const events = require('./_events')(knex, logger);
 
   return {
     users,

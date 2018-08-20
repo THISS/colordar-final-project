d48b94d551195ccdd174427eeeb6332cc6e8131d
Brenton Holswich <brenton.holswich@gmail.com>
2018-08-19 23:14:24 -0700

    cleaned up the events code style for db helper and api route

--- server/db/helpers/_events.js
+++ server/db/helpers/_events.js
@@ -3,18 +3,18 @@ module.exports = function(knex, logger) {
   const TABLE_NAME = 'events';
 
   // functions for the database go here
-  const addEvent = (eventObj) => {
+  const addEvent = eventObj => {
     return knex.insert(eventObj, ['id', 'color_id', 'calendar_id', 'name', 'start_time', 'end_time', 'location'])
       .into(TABLE_NAME);
   };
 
-  const deleteEvent = (eventId) => {
+  const deleteEvent = eventId => {
     return knex.del()
       .from(TABLE_NAME)
-      .where({id: eventId});
+      .where({ id: eventId });
   };
 
-  const getAllCalendarEvents = (calendarId) => {
+  const getAllCalendarEvents = calendarId => {
     return knex.select(
       'events.id',
       'color_id',
@@ -27,8 +27,8 @@ module.exports = function(knex, logger) {
       .where({ calendar_id: calendarId });
   };
 
-  const getEventById = (eventId) => {
-    return knex.first(
+  const getEventById = eventId => {
+    return knex.first([
       'id',
       'color_id',
       'calendar_id',
@@ -36,15 +36,15 @@ module.exports = function(knex, logger) {
       'start_time',
       'end_time',
       'location'
-    )
+    ])
     .from(TABLE_NAME)
-    .where({id: eventId});
+    .where({ id: eventId });
   };
 
-  const getMasterCalendarEvents = (userId) => {
+  const getMasterCalendarEvents = userId => {
     // need to take user id in master_calendars and use the calendar ids where merged is true
     // need to get all events where the calendar_id is in the calendar ids
-    return knex.select(
+    return knex.select([
       'events.id',
       'color_id',
       'events.calendar_id',
@@ -52,7 +52,7 @@ module.exports = function(knex, logger) {
       'start_time',
       'end_time',
       'location'
-    )
+    ])
       .from(TABLE_NAME)
       .innerJoin('master_calendars', 'events.calendar_id', '=', 'master_calendars.calendar_id')
       .innerJoin('users', 'master_calendars.user_id', '=', 'users.id')
@@ -62,13 +62,13 @@ module.exports = function(knex, logger) {
       });
   };
 
-  const getUsersCalendars = (userId) => {
-    return knex.select(
+  const getUsersCalendars = userId => {
+    return knex.select([
       'calendars.id',
       'name',
       'color_id',
       'merged'
-      )
+    ])
       .from('calendars')
       .innerJoin('master_calendars', 'calendars.id', '=', 'master_calendars.calendar_id')
       .where({
@@ -79,7 +79,7 @@ module.exports = function(knex, logger) {
   const updateEvent = (eventId, eventObj) => {
     return knex(TABLE_NAME)
       .update(eventObj)
-      .where({id: eventId})
+      .where({ id: eventId })
       .returning([
           'id',
           'color_id',
--- server/routes/_eventsApi.js
+++ server/routes/_eventsApi.js
@@ -5,39 +5,39 @@ module.exports = (db, log) => {
   
   const errorHandler = (error, res) => {
     log.error(error);
-    res.json({ error: 'something funky went down' });
+    return res.json({ error: 'something funky went down' });
   };
 
   router.get('/',(req, res) => {
-    const calId = Number(req.query.calid);
+    const calId = req.query.calid;
     
     const isMaster = req.query.ismaster;
-    const userId = 1; // TODO: add the users id here
+    const userId = req.user.id;
     const responseObj = {};
 
     if (isMaster) {
       // Pass in the user id
       db.getMasterCalendarEvents(userId)
-      .then((queryResponse) => {
+      .then(queryResponse => {
         responseObj.events = queryResponse;
-        return db.getUsersCalendars(userId)
+        return db.getUsersCalendars(userId);
       })
-      .then((queryResponse) => {
+      .then(queryResponse => {
         responseObj.calendars = queryResponse;
-        res.json(responseObj);
+        return res.json(responseObj);
       })
-      .catch((error) => {
+      .catch(error => {
         errorHandler(error, res);
       });
 
       // TODO: make a pagination for the above query
     }else {
       db.getAllCalendarEvents(calId)
-        .then((queryResponse) => {
+        .then(queryResponse => {
           responseObj.events = queryResponse;
-          res.json(responseObj);
+          return res.json(responseObj);
         })
-        .catch((error) => {
+        .catch(error => {
           errorHandler(error, res);
       });
     }
@@ -47,10 +47,10 @@ module.exports = (db, log) => {
     const eventInput = req.body;
     
     db.addEvent(eventInput)
-      .then((queryResponse) => {
-        res.json(queryResponse[0]);
+      .then(queryResponse => {
+        return res.json(queryResponse[0]);
       })
-      .catch((error) => {
+      .catch(error => {
         errorHandler(error, res);
       });
   });
@@ -62,10 +62,10 @@ module.exports = (db, log) => {
     }
 
     db.getEventById(eventId)
-      .then((queryResponse) => {
-        res.json(queryResponse);
+      .then(queryResponse => {
+        return res.json(queryResponse);
       })
-      .catch((error) => {
+      .catch(error => {
         errorHandler(error, res);
       });
   });
@@ -79,25 +79,26 @@ module.exports = (db, log) => {
     const eventObj = req.body;
 
     db.updateEvent(eventId, eventObj)
-      .then((queryResponse) => {
+      .then(queryResponse => {
         // queryResponse return an array of updated rows - we just changed 1
-        res.json(queryResponse[0]);
+        return res.json(queryResponse[0]);
       })
-      .catch((error) => {
+      .catch(error => {
         errorHandler(error, res);
       });
   });
 
-  router.delete('/', (req, res) => {
-    const eventId = Number(req.body.id);
+  router.delete('/:id', (req, res) => {
+    const eventId = req.params.id;
     if (!eventId) {
       errorHandler('no eventId given', res);
     }
+
     db.deleteEvent(eventId)
-      .then((success) => {
-        res.json({deleted: true, id: eventId});
+      .then(success => {
+        return res.json({ deleted: true, id: eventId });
       })
-      .catch((error) => {
+      .catch(error => {
         errorHandler(error, res);
       });
   })
